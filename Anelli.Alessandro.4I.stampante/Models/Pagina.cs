c864964bbb9f09d154957fdffb39917b05781037
Alessandro Anelli <127589937+APA2306@users.noreply.github.com>
2023-12-04 17:52:17 +0100

    Add files via upload

---
+++ Anelli.Alessandro.4I.stampante/Models/Pagina.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Anelli.Alessandro._4I.stampante
+{
+    public class Pagina
+    {
+        private int nero;
+        private int ciano;
+        private int giallo;
+        private int magenta;
+
+        public int Nero { get => nero; set => nero = value; }
+        public int Ciano { get => ciano; set => ciano = value; }
+        public int Giallo { get => giallo; set => giallo = value; }
+        public int Magenta { get => magenta; set => magenta = value; }
+
+        public Pagina(int N, int C, int G, int M)
+        {
+            if (N <= 3 && C <= 3 && G <= 3 && M <= 3 && N > 0 && C > 0 && G > 0 && M > 0)
+            {
+                Nero = N;
+                Ciano = C;
+                Giallo = G;
+                Magenta = M;
+            }
+            else
+            {
+                MessageBox.Show("I valori devono essere compresi fra 0 e 3");
+                throw new ArgumentException();
+            }
+
+        }
+        public Pagina()
+        {
+            Random random = new Random();
+            Nero = random.Next(3);
+            Ciano = random.Next(3);
+            Giallo = random.Next(3);
+            Magenta = random.Next(3);
+        }
+    }
+}
---
+++ Anelli.Alessandro.4I.stampante/Models/Stampante.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Anelli.Alessandro._4I.stampante
+{
+    public class Stampante
+    {
+        public int Ciano { get; set; }
+        public int Nero { get; set; } 
+        public int Giallo { get; set; }
+        public int Magenta { get; set; }
+        public int Fogli { get; set; }
+
+
+        public Stampante()
+        {
+            Ciano = Nero = Giallo = Magenta = 100;
+            Fogli = 200;
+        }
+
+        public bool Stampa(Pagina pagina)
+        {
+            if(Ciano - pagina.Ciano > 0 && Nero - pagina.Nero > 0 && Giallo - pagina.Giallo > 0 && Magenta - pagina.Magenta > 0 && Fogli - 1 > 0)
+            {
+                Ciano = Ciano - pagina.Ciano;
+                Nero = Nero - pagina.Nero;
+                Giallo = Giallo - pagina.Giallo;
+                Magenta = Magenta - pagina.Magenta;
+                Fogli = Fogli--;
+                return true;
+            }
+            else
+            {
+                return false;
+            }  
+        }
+        public int StatoInchiostro(string C) 
+        {
+            if (C.ToLower() == "Ciano")
+                return Ciano;
+            else if (C.ToLower() == "nero")
+                return Nero;
+            else if (C.ToLower() == "giallo")
+                return Giallo;
+            else if (C.ToLower() == "magenta")
+                return Magenta;
+            else
+            {
+                MessageBox.Show("Colore invalido");
+                return 0;
+            }
+        }
+        public void SostituisciColore(String C)
+        {
+            if (C.ToLower() == "ciano")
+                Ciano = 100;
+            else if (C.ToLower() == "nero")
+                Nero = 100;
+            else if (C.ToLower() == "giallo")
+                Giallo = 100;
+            else if (C.ToLower() == "magenta")
+                Magenta = 100;
+            else
+            {
+                MessageBox.Show("Tipo di colore invalido.");
+                return;
+            }
+        }
+        public int StatoCarta()
+        {
+            return Fogli;
+        }
+        public void AggiungiCarta(int q)
+        {
+            if (q > 0)
+            {
+                MessageBox.Show("Impossibile caricare un numero negativo di carta");
+                return;
+            }
+            else if (Fogli + q <= 200)
+                Fogli = Fogli + q;
+            else
+                Fogli = 200;
+
+            MessageBox.Show("Carta caricata");
+
+        }
+    }
+}
+    
